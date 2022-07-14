063da3409c4dec0de5f45a868345dcba7277d69b
Bruno Fraga <brunocrfraga@gmail.com>
2022-07-14 16:49:21 -0300

    Revert "Security config"
    This reverts commit bb2a903744cdcc6334724c73005f4ff4e25dfc72.
    
    Commit com erro

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import NotificationButton from "./components/NotificationButton";
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div
+            className="dsmeta-container">
+              <SalesCard/>
+          </div>
+        </section>
+      </main>
+    </>
+  )
+
+
+
+}
+
+export default App;
